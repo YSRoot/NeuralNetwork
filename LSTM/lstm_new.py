711857185822d67c288cd5914bb0081ff04bb6cd
Uvays <ualeroev@bk.ru>
2018-12-21 18:35:45 +0300

    add comments and examples for encode/decode

--- LSTM/lstm_new.py
+++ LSTM/lstm_new.py
@@ -5,25 +5,60 @@ Created on Mon Dec 17 17:25:52 2018
 @author: Xiaomi
 """
 
-import pandas as pd
 import numpy as np
+import sys
+sys.path.append("/")
+import encoder
 
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 
+# default url for negative positive and dictionary files
+positive, negative, dictionary = ('data/positive.csv','data/negative.csv','data/dictionary.csv')
+#encoder = encoder.encoder_twitter(dictionary)
 
-pos = pd.read_csv('positive_set.csv',sep=';',index_col='index')
-neg = pd.read_csv('negative_set.csv',sep=';',index_col='index')
+encoder = encoder.encoder_twitter()
+# if there are no encoded files uncomment the following lines
 
-pos = pos + 1
-neg = neg + 1 
-pos = pos.fillna(0)
-neg = neg.fillna(0)
+#encoder.encode(data, url,limit,save)
+# data - 2D array Numpy ot list
+# url - twitts file
+# limit - # limit - upper limit for dataset (more quickly train)
+# save - overwrites files after encoding
+
+pos = encoder.encode(positive, save=True)
+neg = encoder.encode(negative, save=True)
+
+# and comment on the following 2 lines
+
+# read  positive and negative dataset
+#pos = pd.read_csv('data/positive_set.csv',sep=';',index_col='index')
+#neg = pd.read_csv('data/negative_set.csv',sep=';',index_col='index')
+
+# u can add your twitts in dataset
+# example
+#twitts = [
+#    ['Че за ***, ну такой себе фильм'],
+#    ['Потрясающий фильм схожу еще раз'],
+#    [' фигня'],
+#    ['день сегодня прекрасный, вот бы выспаться еще'],
+#    ['Ненавижу этот мир, поубивал бы всех тут'],
+#    ['Сон это дар богов']
+#]
+
+#new_pos = encoder.encode(data=twitts)
+#new_pos = sequence.pad_sequences(new_pos,len(pos))
+#pos = np.r_[new_pos,pos]
 #
-pos = sequence.pad_sequences(pos.values, maxlen=40)
-neg = sequence.pad_sequences(neg.values, maxlen=40)
+#new_neg = encoder.encode(data=twitts)
+#new_neg = sequence.pad_sequences(new_pos,len(pos))
+#neg = np.r_[new_neg,neg]
+
+
+pos = sequence.pad_sequences(pos.values, maxlen = 40)
+neg = sequence.pad_sequences(neg.values, maxlen = 40)
 
 pos = np.c_[pos,np.ones(len(pos))]
 neg = np.c_[neg,np.zeros(len(neg))]
@@ -50,7 +85,6 @@ model.add(Embedding(max_features, 128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 
-# try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
@@ -60,3 +94,14 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=5,
           validation_data=(x_test, y_test))
+
+#test NN
+print(model.predict(x_test))
+
+#test = encoder.encode(data=twitts)
+#test = sequence.pad_sequences(test,maxlen = 40)
+#print(model.predict(test))
+
+# example decode
+#decode_array = encoder.decode(x_test)
+#print(decode_array)
