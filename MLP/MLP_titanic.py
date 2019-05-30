a25316a6e476024c1d746379860cefe1b4c73e92
rootis69 <ualeroev@bk.ru>
2019-05-30 12:56:17 +0300

    Mlp for NP301

--- MLP/MLP_titanic.py
+++ MLP/MLP_titanic.py
@@ -2,36 +2,39 @@
 import numpy as np
 import scipy.special as sp
 import matplotlib.pyplot as plt
-# библиотека для работы с csv и dataframe
+# библиотека для работы с dataframe
 import pandas as pd
 
 # активационная функция (1/(e^(-x)))
 def f(x):
     return sp.expit(x)
+
 # производная активационной функции
 def f1(x):
-    return x*(1-x)
-#функция инициализации весов
+    return x * (1 - x)
+
+# функция инициализации весов
 # на вход имеет несколько аргументов:
 #    inputs - количество входных узлов
 #    hiddens - количество узлов 1го скрытого слоя
 #    hiddens2 - количество узлов 2го скрытого слоя
 #    outputs - количество узлов выходного слоя
-def init_weight(inputs,hiddens,hiddens2, outputs):
+def init_weight(inputs, hiddens, hiddens2, outputs):
 #    матрица весов от входного слоя к 1му скрытому слою
 #    принимает рандомные значения и имеет размерность [inputs х hiddens]
-    w1 = np.random.random((inputs,hiddens))
+    w1 = np.random.random((inputs, hiddens))
 #    матрица весов от 1го скрытого слоя к 2му скрытому слою
 #    принимает рандомные значения и имеет размерность [hiddens+1 х hiddens2]
 #    эта матрица имеет кол-во строк на единицу больше т.к. нужно учитывать мнимую единицу
-    w2 = np.random.random((hiddens+1,hiddens2))
+    w2 = np.random.random((hiddens + 1, hiddens2))
 #    матрица весов от 2го скрытого слоя к выходному слою
 #    принимает рандомные значения и имеет размерность [hiddens2+1 х outputs]
 #    эта матрица имеет кол-во строк на единицу больше т.к. нужно учитывать мнимую единицу
-    w3 =  np.random.random((hiddens2+1,outputs))
-    return w1,w2,w3
+    w3 = np.random.random((hiddens2 + 1, outputs))
+    return w1, w2, w3
 
-#функция тренировки сети
+
+# функция тренировки сети
 # на вход имеет несколько аргументов:
 #    inputs_list - обучающее множество (входные сигналы)
 #    w1 - матрица весов от входного слоя к 1му скрытому слою
@@ -40,23 +43,23 @@ def init_weight(inputs,hiddens,hiddens2, outputs):
 #    targets_list - целевое множество
 #    lr - скорость обучения сети
 #    error - допустимая погрешность в обучении
-def train(inputs_list,w1,w2,w3,targets_list,lr,error):
-#    счетчик эпох
+def train(inputs_list, w1, w2, w3, targets_list, lr, error):
+    #    счетчик эпох
     era = 0
-#    глобальная ошибка
+    #    глобальная ошибка
     global_error = 1
-#    список ошибок
+    #    список ошибок
     list_error = []
-#   �"лавный цикл обучения, повторяется пока глобальная ошибка больше погрешности
-    while global_error>error:
-#        локальная ошибка 
+    #   �"лавный цикл обучения, повторяется пока глобальная ошибка больше погрешности
+    while global_error > error:
+        #        локальная ошибка
         local_error = np.array([])
         # побочный цикл, прогоняющий данные с input_list
         # функция enumerate(matrix) возвращает индекс и значение строк
         # которая сохраняется в переменные i, value
         # i - индекс строки input_list
         # value - переменная которая хранит в себе строки матрицы input_list
-        for i,inputs in enumerate(inputs_list):
+        for i, inputs in enumerate(inputs_list):
             # переводит листа inputs в двумерный вид (для возможности проведения операции транспонирования)
             inputs = np.array(inputs, ndmin=2)
 #            targets - содержит локальный таргет для данного инпута
@@ -64,75 +67,79 @@ def train(inputs_list,w1,w2,w3,targets_list,lr,error):
 
 #            прямое распространение
 #            скалярное произведение строки на матрицу весов
-            hidden_in = np.dot(inputs,w1)
+            hidden_in = np.dot(inputs, w1)
 #            применение активационной функции к вектору
             hidden_out = f(hidden_in)
 #            добавление в начало вектора мнимой единицы для обучения сети
-            hidden_out = np.array(np.insert(hidden_out,0,[1]), ndmin=2)
-            
+            hidden_out = np.array(np.insert(hidden_out, 0, [1]), ndmin=2)
+
 #            скалярное произведение строки на матрицу весов
-            hidden_in2 = np.dot(hidden_out,w2)            
+            hidden_in2 = np.dot(hidden_out, w2)
 #            применение активационной функции к вектору
             hidden_out2 = f(hidden_in2)
 #            добавление в начало вектора мнимой единицы
-            hidden_out2 = np.array(np.insert(hidden_out2,0,[1]), ndmin=2)
-            
+            hidden_out2 = np.array(np.insert(hidden_out2, 0, [1]), ndmin=2)
+
 #            скалярное произведение строки на матрицу весов
-            final_in = np.dot(hidden_out2,w3)
+            final_in = np.dot(hidden_out2, w3)
 #            активационная функция выходного слоя это прямая y = x, поэтому
 #            здесь значение "out" равно значение "in"
             final_out = final_in
+            
+            
 #            вычисление ошибки выходного слоя
             output_error = targets - final_out
 #            вычисление ошибки второго скрытого слоя
-            hidden_error2 = np.dot(output_error,w3.T)
+            hidden_error2 = np.dot(output_error, w3.T)
 #            вычисление ошибки первого скрытого слоя
-            hidden_error = np.dot(hidden_error2[:,1:],w2.T)
+            hidden_error = np.dot(hidden_error2[:, 1:], w2.T)
 #            добавление в список локальных ошибок текущую ошибку
             local_error = np.append(local_error, output_error)
 #            обратного распространение ошибки
 #            изменение матрицы весов 3 т.к. производная активационный функции (y = x)
 #            y` = 1 в dW = lr*output_error*hidden_out2.T не умножается на эту производную
-            w3 += lr*output_error*hidden_out2.T
+            w3 += lr * output_error * hidden_out2.T
 #            в методе обратного распространения ошибки исключается мнимая единичка для совпадения размерностей
 #            hidden_error2[:,1:] - означает весь вектор за исключением первого элемента
-            w2 += lr*hidden_error2[:,1:]*f1(hidden_out2[:,1:])*hidden_out.T
-            w1 += lr*hidden_error[:,1:]*f1(hidden_out[:,1:])*inputs.T
+            w2 += lr * hidden_error2[:, 1:] * f1(hidden_out2[:, 1:]) * hidden_out.T
+            w1 += lr * hidden_error[:, 1:] * f1(hidden_out[:, 1:]) * inputs.T
 #        глобальная ошибка - это средняя по модуля от всех локальных ошибок
         global_error = abs(np.mean(local_error))
 #        global_error = np.sqrt(((local_error) ** 2).mean())
 #        эпоха увеличивается на 1
-        era+=1
+        era += 1
 #        вывод в консоль текущую глобальную ошибку
         print(global_error)
 #        в список ошибок добавляется глобальная ошибка
         list_error.append(global_error)
 #        если при обучении количество эпох превысит порог 10000 то обучение прекратится
-        if era >10000: break
+        if era > 10000: break
 #    возвращает измененные веса, количество эпох, и список ошибок
-    return w1,w2,w3,era,list_error
+    return w1, w2, w3, era, list_error
+
 
 # функция для проверки обученной сети и вывода результата
-def query(inputs_list, w1,w2,w3):
+def query(inputs_list, w1, w2, w3):
 #    создаем список в котором будем хранить "outs" для тестового множества
     final_out = np.array([])
-    for i,inputs in enumerate(inputs_list):
+    for i, inputs in enumerate(inputs_list):
 #       прямое распространение так же как и при обучении для получении "out"
         inputs = np.array(inputs, ndmin=2)
-        
-        hidden_in = np.dot(inputs,w1)
-        hidden_out = f(hidden_in)
-        hidden_out = np.array(np.insert(hidden_out,0,[1]), ndmin=2)
-        
-        hidden_in2 = np.dot(hidden_out,w2)
-        hidden_out2 = f(hidden_in2)
-        hidden_out2 = np.array(np.insert(hidden_out2,0,[1]), ndmin=2)
-        
-        final_in = np.dot(hidden_out2,w3)
 
-        final_out = np.append(final_out,final_in)
-#    возвращаем значение вектора "out" округленные до целого числа            
+        hidden_in = np.dot(inputs, w1)
+        hidden_out = f(hidden_in)
+        hidden_out = np.array(np.insert(hidden_out, 0, [1]), ndmin=2)
+
+        hidden_in2 = np.dot(hidden_out, w2)
+        hidden_out2 = f(hidden_in2)
+        hidden_out2 = np.array(np.insert(hidden_out2, 0, [1]), ndmin=2)
+
+        final_in = np.dot(hidden_out2, w3)
+
+        final_out = np.append(final_out, final_in)
+#    возвращаем значение вектора "out" округленные до целого числа
     return np.around(final_out)
+
 
 # считываем данные с csv с помощью библиотеки pandas
 # Данные о пассажирах Титаник
@@ -143,7 +150,7 @@ data = pd.read_csv('titanic_data.csv', index_col='PassengerId')
 # .values означает что данные из dataframe конвертируются в numpy array
 target_data = data['Survived'].values
 # удаляем из датасета столбец Survived и конвертируем в array
- #data = data.drop(columns=['Survived']).values
+# data = data.drop(columns=['Survived']).values
 data = data.drop('Survived', 1).values
 
 # составляем выборку обучающего множества из первых 600 строк датасета
@@ -173,7 +180,8 @@ hidden_layer2 = 4
 # количество узлов в выходном слое
 output_layer = 1
 
-# инициализация весов в зависимости от количества узлов в слоях сети
+# инициop[
+ализация весов в зависимости от количества узлов в слоях сети
 w1, w2, w3 = init_weight(input_layer, hidden_layer, hidden_layer2, output_layer)
 
 # тренировка сети
@@ -197,4 +205,3 @@ print("Точность - " + str(eq))
 fig = plt.figure(figsize=(20,20))
 plt.plot(np.arange(era),lst)
 plt.show()
-
